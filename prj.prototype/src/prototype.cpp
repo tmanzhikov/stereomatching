9fdf7e25c272c7968958aef037fe7842a9facc80
tmanzihkov <temudzhin94@gmail.com>
2017-04-04 19:54:26 +0300

    repo created

---
+++ prj.prototype/src/prototype.cpp
@@ -0,0 +1,360 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+const string save_path = "../output/";
+typedef unsigned long long bitmap;
+
+uchar compute_hamming_distance(const bitmap& a, const bitmap& b) {
+  uchar ret = 0;
+  bitmap c = a ^ b;
+  for (size_t i = 0; i < 48; ++i) {
+    ret += bool((1LL << i) & c);
+  }
+  return ret;
+}
+
+vector<uchar> compute_census(const Mat& img, const int& y,
+                             const int& x, const int& k) {
+  vector<uchar> ret(48);
+
+  uchar val = img.ptr(y)[x];
+  int id = 0;
+
+  for (int i = y - k; i < y + k + 1; ++i) {
+    const uchar* p = img.ptr(i);
+    for (int j = x - k; j < x + k + 1; ++j) {
+      if (i == y && j == x)
+        continue;
+      if (p[j] < val)
+        ret[id] = 1;
+      id++;
+    }
+  }
+  return ret;
+}
+
+bitmap compute_census_bitmap(const Mat& img, const int& y,
+                             const int& x, const int& k) {
+  bitmap ret = 0;
+
+  uchar val = img.ptr(y)[x];
+  int id = 0;
+
+  for (int i = y - k; i < y + k + 1; ++i) {
+    const uchar* p = img.ptr(i);
+    for (int j = x - k; j < x + k + 1; ++j) {
+      if (i == y && j == x)
+        continue;
+      if (p[j] < val)
+        ret |= (1ll << id);
+      id++;
+    }
+  }
+  return ret;
+}
+
+void log_disp_map(const vector<vector<vector<int> > >& cost, string nm) {
+
+  size_t max_d = cost.size();
+  size_t h = cost[0].size();
+  size_t w = cost[0][0].size();
+
+  Mat ret(Mat::zeros(h, w, CV_32SC1));
+
+  for (size_t y = 0; y < h; ++y) {
+    for (size_t x = 0; x < w; ++x) {
+      int d_cost = cost[0][y][x];
+      int d_val  = 0;
+      for (size_t d = 1; d < max_d; ++d) {
+        if (cost[d][y][x] < d_cost) {
+          d_cost = cost[d][y][x];
+          d_val = d;
+        }
+      }
+      ret.at<int>(y, x) = d_val;
+    }
+  }
+
+  normalize(ret, ret, 0, 255, NORM_MINMAX, CV_32SC1);
+  Mat ret_normalized(Mat::zeros(h, w, CV_8UC1));
+
+  ret.convertTo(ret_normalized, CV_8UC1);
+
+  imwrite(save_path + "normalized_disp_map_" + nm + ".png", ret_normalized);
+
+  medianBlur(ret_normalized, ret_normalized, 3);
+  imwrite(save_path + "blurred_disp_map_" + nm + ".png", ret_normalized);
+}
+
+vector<vector<vector<int> > > compute_census_rand_basic_cost(
+    const Mat& b, const Mat& m, int max_d, int k = 3) {
+  int w = b.cols;
+  int h = b.rows;
+
+  //vector<vector<vector<uchar> > > fb(h, vector<vector<uchar> >(w));
+  //vector<vector<vector<uchar> > > fm(h, vector<vector<uchar> >(w));
+  vector<vector<bitmap> > fb(h, vector<bitmap>(w));
+  vector<vector<bitmap> > fm(h, vector<bitmap >(w));
+
+  vector<vector<vector<int> > > basic_cost(max_d,
+      vector<vector<int> >(h, vector<int>(w, 100000)));
+
+  // k - window's wing size
+  for (int y = k; y < h - k; ++y) {
+    for (int x = k; x < w - k; ++x) {
+      //fb[y][x] = compute_census(b, y, x, k);
+      //fm[y][x] = compute_census(m, y, x, k);
+      fb[y][x] = compute_census_bitmap(b, y, x, k);
+      fm[y][x] = compute_census_bitmap(m, y, x, k);
+
+    }
+  }
+
+  Mat disp_map(Mat::zeros(h, w, CV_8UC1));
+
+  for (int y = k; y < h - k; ++y) {
+    for (int x = k ; x < w - k; ++x) {
+      int value = 0;
+      uchar min_dist = 100;
+      for (int d = - max_d / 2; d < max_d / 2; ++d) {
+        int x_d = x + d;
+        if (x_d < k + 1 || x_d > w - k - 1)
+          continue;
+        uchar cur_dist = compute_hamming_distance(fb[y][x], fm[y][x_d]);
+        if (cur_dist < min_dist) {
+          min_dist = cur_dist;
+          value = d;
+        }
+        basic_cost[d + max_d / 2][y][x] = cur_dist;
+      }
+      disp_map.at<uchar>(y, x) = value + max_d / 2;
+    }
+  }
+  return basic_cost;
+}
+
+
+void aggregate_cost(vector<vector<vector<int> > >& sum,
+                    vector<vector<vector<int> > >& a) {
+  int max_val = 0;
+  for (size_t i = 0; i < sum.size(); ++i) {
+    for (size_t j = 0; j < sum[0].size(); ++j) {
+      for (size_t k = 0; k < sum[0][0].size(); ++k) {
+        sum[i][j][k] += a[i][j][k];
+        max_val = std::max(a[i][j][k], max_val);
+        a[i][j][k] = 0;
+      }
+    }
+  }
+}
+
+
+void compute_cost_aggregation(const vector<vector<vector<int> > >& basic_cost,
+                              int p1, int p2) {
+
+  log_disp_map(basic_cost, "0_basic_cost");
+
+  size_t max_d = basic_cost.size();
+  size_t h = basic_cost[0].size();
+  size_t w = basic_cost[0][0].size();
+
+  vector<vector<vector<int> > > cur_cost(max_d,
+      vector<vector<int> >(h, vector<int>(w, 0)));
+  vector<vector<vector<int> > > sum_cost(max_d,
+      vector<vector<int> >(h, vector<int>(w, 0)));
+
+  const int inf = numeric_limits<int>::max();
+
+  // left to right
+  for (size_t y = 0; y < h; ++y) {
+    for (size_t x = 0; x < w; ++x) {
+      for (size_t d = 0; d < max_d; ++d) {
+        int cur_val = basic_cost[d][y][x];
+
+        if (x == 0) {
+          cur_cost[d][y][x] = cur_val;
+          continue;
+        }
+
+        int m1 = cur_cost[d][y][x - 1];
+        int m2 = (d != max_d - 1 ? cur_cost[d + 1][y][x - 1] + p1 : inf);
+        int m3 = (d != 0         ? cur_cost[d - 1][y][x - 1] + p1 : inf);
+
+        int min_pred = numeric_limits<int>::max();
+        for (size_t temp_d = 0; temp_d < max_d; ++temp_d) {
+          min_pred = std::min(min_pred, cur_cost[temp_d][y][x - 1]);
+        }
+        int m4 = min_pred + p2;
+
+        cur_val += std::min(m1,
+                   std::min(m2,
+                   std::min(m3,
+                            m4)));
+        cur_val -= min_pred;
+        cur_cost[d][y][x] = cur_val;
+      }
+    }
+  }
+  log_disp_map(cur_cost, "self_1");
+  aggregate_cost(sum_cost, cur_cost);
+  log_disp_map(sum_cost, "aggregated_1");
+
+  // right to left
+  for (size_t y = 0; y < h; ++y) {
+    for (int x = w - 1; x >= 0; --x) {
+      for (size_t d = 0; d < max_d; ++d) {
+        int cur_val = basic_cost[d][y][x];
+
+        if (x == int(w - 1)) {
+          cur_cost[d][y][x] = cur_val;
+          continue;
+        }
+
+        int m1 = cur_cost[d][y][x + 1];
+        int m2 = (d != max_d - 1 ? cur_cost[d + 1][y][x + 1] + p1 : inf);
+        int m3 = (d != 0         ? cur_cost[d - 1][y][x + 1] + p1 : inf);
+
+        int min_pred = numeric_limits<int>::max();
+        for (size_t temp_d = 0; temp_d < max_d; ++temp_d) {
+          min_pred = std::min(min_pred, cur_cost[temp_d][y][x + 1]);
+        }
+        int m4 = min_pred + p2;
+
+        cur_val += std::min(m1,
+                   std::min(m2,
+                   std::min(m3,
+                            m4)));
+        cur_val -= min_pred;
+        cur_cost[d][y][x] = cur_val;
+      }
+    }
+  }
+  log_disp_map(cur_cost, "self_2");
+  aggregate_cost(sum_cost, cur_cost);
+  log_disp_map(sum_cost, "aggregated_2");
+
+  // to top left
+  for (size_t y = 0; y < h; ++y) {
+    for (size_t x = 0; x < w; ++x) {
+      for (size_t d = 0; d < max_d; ++d) {
+        int cur_val = basic_cost[d][y][x];
+
+        if (x == 0 || y == 0) {
+          cur_cost[d][y][x] = cur_val;
+          continue;
+        }
+
+        int m1 = cur_cost[d][y - 1][x - 1];
+        int m2 = (d != max_d - 1 ? cur_cost[d + 1][y - 1][x - 1] + p1 : inf);
+        int m3 = (d != 0         ? cur_cost[d - 1][y - 1][x - 1] + p1 : inf);
+
+        int min_pred = numeric_limits<int>::max();
+        for (size_t temp_d = 0; temp_d < max_d; ++temp_d) {
+          min_pred = std::min(min_pred, cur_cost[temp_d][y - 1][x - 1]);
+        }
+        int m4 = min_pred + p2;
+
+        cur_val += std::min(m1,
+                   std::min(m2,
+                   std::min(m3,
+                            m4)));
+        cur_val -= min_pred;
+        cur_cost[d][y][x] = cur_val;
+      }
+    }
+  }
+  log_disp_map(cur_cost, "self_3");
+  aggregate_cost(sum_cost, cur_cost);
+  log_disp_map(sum_cost, "aggregated_3");
+
+  // to top right
+  for (size_t y = 0; y < h; ++y) {
+    for (int x = w - 1; x >= 0; --x) {
+      for (size_t d = 0; d < max_d; ++d) {
+        int cur_val = basic_cost[d][y][x];
+
+        if (x == int(w - 1) || y == 0) {
+          cur_cost[d][y][x] = cur_val;
+          continue;
+        }
+
+        int m1 = cur_cost[d][y - 1][x + 1];
+        int m2 = (d != max_d - 1 ? cur_cost[d + 1][y - 1][x + 1] + p1 : inf);
+        int m3 = (d != 0         ? cur_cost[d - 1][y - 1][x + 1] + p1 : inf);
+
+        int min_pred = numeric_limits<int>::max();
+        for (size_t temp_d = 0; temp_d < max_d; ++temp_d) {
+          min_pred = std::min(min_pred, cur_cost[temp_d][y - 1][x + 1]);
+        }
+        int m4 = min_pred + p2;
+
+        cur_val += std::min(m1,
+                            std::min(m2,
+                                     std::min(m3,
+                                              m4)));
+        cur_val -= min_pred;
+        cur_cost[d][y][x] = cur_val;
+      }
+    }
+  }
+  log_disp_map(cur_cost, "self_4");
+  aggregate_cost(sum_cost, cur_cost);
+  log_disp_map(sum_cost, "aggregated_4");
+
+  // to top
+  for (size_t y = 0; y < h; ++y) {
+    for (size_t x = 0; x < w; ++x) {
+      for (size_t d = 0; d < max_d; ++d) {
+        int cur_val = basic_cost[d][y][x];
+
+        if (y == 0) {
+          cur_cost[d][y][x] = cur_val;
+          continue;
+        }
+
+        int m1 = cur_cost[d][y - 1][x];
+        int m2 = (d != max_d - 1 ? cur_cost[d + 1][y - 1][x] + p1 : inf);
+        int m3 = (d != 0         ? cur_cost[d - 1][y - 1][x] + p1 : inf);
+
+        int min_pred = numeric_limits<int>::max();
+        for (size_t temp_d = 0; temp_d < max_d; ++temp_d) {
+          min_pred = std::min(min_pred, cur_cost[temp_d][y - 1][x]);
+        }
+        int m4 = min_pred + p2;
+
+        cur_val += std::min(m1,
+                            std::min(m2,
+                                     std::min(m3,
+                                              m4)));
+        cur_val -= min_pred;
+        cur_cost[d][y][x] = cur_val;
+      }
+    }
+  }
+  log_disp_map(cur_cost, "self_5");
+  aggregate_cost(sum_cost, cur_cost);
+  log_disp_map(sum_cost, "aggregated_5");
+}
+
+int main() {
+
+  Mat base = imread("../testdata/cones1.png", CV_LOAD_IMAGE_GRAYSCALE);
+  Mat match = imread("../testdata/cones2.png", CV_LOAD_IMAGE_GRAYSCALE);
+  int max_d = 128;
+
+  if (base.data && match.data) {
+    auto basic_cost = compute_census_rand_basic_cost(base, match, max_d);
+    compute_cost_aggregation(basic_cost, 3, 10);
+  } else {
+    std::cout << "Failed to read images" << std::endl;
+  }
+  return 0;
+}
---
+++ prj.stereomatching/src/stereomatching.cpp
@@ -0,0 +1,291 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+namespace sm {
+
+const std::string save_path = "../output/";
+typedef unsigned long long       bitmap;
+typedef short                    cost_type;
+typedef std::vector<cost_type>   buffer_line;
+typedef std::vector<buffer_line> buffer;
+
+struct Settings {
+  int census_wing; // size of census rank kernel
+  int census_size; //
+  int max_d;       // maximal disparity value
+  int width;       // image width
+  int p1;          //
+  int p2;          //
+  Settings () {
+    census_wing = 3;
+    census_size = census_wing * 2 + 1;
+    max_d = 128;
+    width = -1;
+    p1    = 3;
+    p2    = 10;
+  }
+};
+
+class StereoMatcher {
+
+public:
+  StereoMatcher () {}
+  // Computes disparity map of two images.
+  void Process(const cv::Mat& base, const cv::Mat& match);
+
+private:
+  // Computes disparity line by line
+  void ProcessLine(const uchar* base, const uchar* match, uchar* res, int y);
+  // Computes census rank transformation for one line.
+  void InitializeCensusRank(int y);
+  // Recomputes census rank for upcoming line.
+  void RecomputeCensusRank();
+  // Aggregates cost in given direction
+  void AggregateCost(int x_from, int x_to, int delta_x,
+                     int x_dir, int x_edge, int y_cur, int y_prev);
+
+private:
+  /// Resources:
+  Settings settings_;
+  // [census_size][width] arrays storing last census_size lines of image needed
+  // to compute census rank transformation
+  cv::Mat base_buffer_;
+  cv::Mat match_buffer_;
+
+  // Census rank for base image line
+  std::vector<bitmap> base_census_;  
+  // Census rank for match image line
+  std::vector<bitmap> match_census_;
+  // [max_d][width] array storing basic cost 
+  buffer basic_cost_;
+  // [max_d][width] array storing aggregated cost
+  buffer aggregated_cost_;
+  // [2][max_d][width] array storing aggregated cost for previous and current 
+  // lines
+  std::vector<buffer> aggregation_buffer_;
+};
+
+void StereoMatcher::Process(const cv::Mat& base, const cv::Mat& match) {
+  /// 1. Initialization
+  // reading input images
+
+  // resulting image. will be filled line by line
+  cv::Mat result(cv::Mat::zeros(base.rows, base.cols, base.type()));
+
+  // initializing resources
+  base_buffer_ = cv::Mat::zeros(settings_.census_size, base.cols, base.type());
+  match_buffer_ = cv::Mat::zeros(settings_.census_size, base.cols, base.type());
+  settings_.width = base.cols;
+
+  const cost_type max_basic_cost = settings_.census_size * settings_.census_size;
+  basic_cost_.assign(settings_.max_d, 
+                     buffer_line(settings_.width, max_basic_cost));
+
+  aggregated_cost_.assign(settings_.max_d, buffer_line(settings_.width, 0));
+  aggregation_buffer_.assign(2,
+      buffer(settings_.max_d, buffer_line(settings_.width, 0)));
+
+  /// 2. Processing
+  for (size_t y = 0; y < size_t(base.rows); ++y) {
+    ProcessLine(base.ptr(y), match.ptr(y), result.ptr(y), y);
+  }
+
+  /// 3. Saving answer
+  cv::normalize(result, result, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  cv::imwrite(save_path + "disparity_map.png", result);
+
+  // will be impossible to apply median filter with online algorithm,
+  // without little tricks
+  cv::Mat result_blurred;
+  cv::medianBlur(result, result_blurred, 3);
+  cv::imwrite(save_path + "blurred_disparity_map.png", result_blurred);
+}
+
+cost_type ComputeHammingDistance(const bitmap& a, const bitmap& b) {
+#ifdef _MSC_VER
+  return cost_type(__popcnt64(a ^ b));
+#else 
+  return cost_type(__builtin_popcountll(a ^ b));
+#endif
+};
+
+void StereoMatcher::AggregateCost(int x_from, int x_to, int delta_x,
+                                  int x_dir, int x_edge, int y_cur, int y_prev) {
+
+  static const cost_type inf = std::numeric_limits<cost_type>::max();
+  auto& cur = aggregation_buffer_[y_cur];
+  auto& prev = aggregation_buffer_[y_prev];
+
+  for (int x = x_from; x != x_to; x += delta_x) {
+    for (int d = 0; d < settings_.max_d; ++d) {
+      cost_type cur_val = basic_cost_[d][x];
+
+      if (x == x_edge) {
+        cur[d][x] = cur_val;
+        continue;
+      }
+
+      const cost_type m1 = cur[d][x + x_dir];
+      const cost_type m2 = (d != settings_.max_d - 1 ? 
+                            cur[d + 1][x + x_dir] + settings_.p1 : inf);
+      const cost_type m3 = (d != 0 ?                   
+                            cur[d - 1][x + x_dir] + settings_.p1 : inf);
+
+      cost_type min_pred = inf;
+      for (int temp_d = 0; temp_d < settings_.max_d; ++temp_d) {
+        min_pred = std::min(min_pred, cur[temp_d][x + x_dir]);
+      }
+      const cost_type m4 = min_pred + settings_.p2;
+
+      cur_val += std::min(m1,
+                 std::min(m2,
+                 std::min(m3,
+                          m4)));
+
+      cur_val -= min_pred;
+      cur[d][x] = cur_val;
+      prev[d][x] += cur_val;
+      aggregated_cost_[d][x] += prev[d][x];
+    }
+  }
+}
+
+void StereoMatcher::ProcessLine(const uchar* base, const uchar* match,
+                                uchar* res, int y) {
+
+  int local_y = y % settings_.census_size;
+
+  memcpy(base_buffer_.ptr(local_y), base, settings_.width);
+  memcpy(match_buffer_.ptr(local_y), match, settings_.width);
+
+  if (y < settings_.census_size - 1) {
+    return;
+  } else if (y == settings_.census_size - 1) {
+    InitializeCensusRank(settings_.census_wing);
+  } else {
+    InitializeCensusRank((y + settings_.census_wing) % settings_.census_size);
+    //RecomputeCensusRank();
+  }
+
+  // disparity cost computing
+  const int x_from = settings_.census_wing;
+  const int x_to   = settings_.width - settings_.census_wing;
+  const int d_from = -settings_.max_d / 2;
+  const int d_to   = settings_.max_d / 2;
+  for (int x = x_from; x < x_to; ++x) {
+    for (int d = d_from; d < d_to; ++d) {
+      int x_match = x + d;
+      if (x_match > x_from && x_match < x_to) {
+        basic_cost_[d - d_from][x] =
+            ComputeHammingDistance(base_census_[x], match_census_[x_match]);
+      }
+    }
+  }
+
+  // cost aggregation
+
+  if (y == settings_.census_size - 1) {
+    const int y_0 = y % 2;
+    auto& cur = aggregation_buffer_[y_0];
+    for (int x = x_from; x < x_to; ++x) {
+      for (int d = 0; d < settings_.max_d; ++d) {
+        cur[d][x] = basic_cost_[d][x];
+      }
+    }
+  } else {
+    const int y_0 = y % 2;
+    const int y_1 = 1 - y_0;
+
+    for (auto& line : aggregated_cost_) {
+      std::fill(begin(line), end(line), 0);
+    }
+    // left to right
+    AggregateCost(x_from, x_to, 1, 
+                  -1, x_from, y_0, y_1);
+
+    // right to left
+    AggregateCost(x_to - 1, x_from - 1, -1,
+                  1, x_to - 1, y_0, y_1);
+                  
+    // to left top 
+    AggregateCost(x_from, x_to, 1, 
+                  -1, x_from, y_0, y_0);
+  }
+  // saving answer
+  for (int x = x_from; x < x_to; ++x) {
+    cost_type min_val = std::numeric_limits<cost_type>::max();
+    for (int d = 0; d < settings_.max_d; ++d) {
+      if (aggregated_cost_[d][x] < min_val) {
+        res[x] = d;
+        min_val = cost_type(aggregated_cost_[d][x]);
+      }
+    }
+  }
+}
+
+void StereoMatcher::InitializeCensusRank(int y) {
+  base_census_.resize(settings_.width);
+  match_census_.resize(settings_.width);
+
+  int& wdt = settings_.width;
+  int& cwg = settings_.census_wing;
+  int& sz  = settings_.census_size;
+
+  for (int x = cwg; x < wdt - cwg; ++x) {
+    bitmap base_bitmap = 0;
+    bitmap match_bitmap = 0;
+    int id = 0;
+    const uchar base_val = base_buffer_.at<uchar>(y, x);
+    const uchar match_val = match_buffer_.at<uchar>(y, x);
+
+    for (int i = 0; i < sz; ++i) {
+      const uchar* p_base = base_buffer_.ptr(i);
+      const uchar* p_match = match_buffer_.ptr(i);
+      for (int j = x - cwg; j < x + cwg + 1; ++j) {
+        if (p_base[j] < base_val) {
+          base_bitmap |= (1ll << id);
+        }
+        if (p_match[j] < match_val) {
+          match_bitmap |= (1ll << id);
+        }
+        id++;
+      }
+    }
+    base_census_[x] = base_bitmap;
+    match_census_[x] = match_bitmap;
+  }
+}
+
+void StereoMatcher::RecomputeCensusRank() {
+  // recomputes census rank with O(census_wing) instead of O(census_wing^2)
+}
+
+} // namespace sm
+
+int main(int argc, char* argv[]) {
+
+  std::string path_base;
+  std::string path_match;
+
+  if (argc < 2) {
+    path_base = "../testdata/cones1.png";
+    path_match = "../testdata/cones2.png";
+  } else {
+    path_base = argv[1];
+    path_match = argv[2];
+  }
+  cv::Mat base = cv::imread(path_base, CV_LOAD_IMAGE_GRAYSCALE);
+  cv::Mat match = cv::imread(path_match, CV_LOAD_IMAGE_GRAYSCALE);
+
+  if (base.data && match.data) {
+    sm::StereoMatcher solver;
+    solver.Process(base, match);
+  } else {
+    std::cout << "Failed to read images" << std::endl;
+  }
+  return 0;
+}
